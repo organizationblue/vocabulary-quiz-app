bf54f232430b2a2b35870e6bd9989dc85d5f0d3b
Eetu P <sinun_emailisi@example.com>
2026-02-05 11:55:49 +0200

    Testing branch with a comment

--- backend/src/service/wordService.ts
+++ backend/src/service/wordService.ts
@@ -1,6 +1,7 @@
 import words from '../data/words.json' with { type: 'json'};
 import type { Word } from '../types/words.js';
 
+// function for getting a random word from the json file 
 export const getRandomWord = (): Word => {
     const randomIndex = Math.floor(Math.random() * words.length);
     return words[randomIndex]!;
