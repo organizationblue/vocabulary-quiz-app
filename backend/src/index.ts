49d143ae0b5ae110b82c23b317ba611ba7c66ec1
Elias Jungman <elias.jungman@gmail.com>
2026-03-12 13:17:44 +0200

    add dotenv.config() to load.env file and add .env file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
+import dotenv from 'dotenv';
 import { app } from './app.js';
 
+dotenv.config();
 const PORT = process.env.PORT || 8080;
-
 
 app.listen(PORT, () => {
   console.log(`Backend server is running on http://localhost:${PORT}`);
