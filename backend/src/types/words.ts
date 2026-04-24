abb64745b7d368ae2e876c1b20b56ba3e03da7c3
t0mperi <henritomperi97@gmail.com>
2026-04-24 13:56:17 +0300

    Add multiple language to quiz API

--- backend/src/types/words.ts
+++ backend/src/types/words.ts
@@ -1,4 +1,17 @@
+export type Language = 'english' | 'swedish' | 'finnish';
+
+export const SUPPORTED_LANGUAGES: Language[] = ['finnish', 'english', 'swedish'];
+
+export const isSupportedLanguage = (lang: string): lang is Language =>
+    SUPPORTED_LANGUAGES.includes(lang as Language);
+
 export interface Word {
     english: string;
     finnish: string;
+    swedish: string;
+}
+
+export interface QuizWord {
+    prompt: string;
+    answer: string;
 }
